e2d3078ab7122f4446374bed21ff21b22d6ba6bd
Patryk Wegrzy <wegpat@gmail.com>
2019-04-15 01:02:44 +0200

    Postgres test

--- src/main/java/com/football/assistant/AssistantApplication.java
+++ src/main/java/com/football/assistant/AssistantApplication.java
@@ -1,13 +1,24 @@
 package com.football.assistant;
 
+import com.football.assistant.domain.FootballClub;
+import com.football.assistant.repository.FootballClubRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AssistantApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssistantApplication.class, args);
+	}
+
+	@Bean
+	ApplicationRunner applicationRunner(FootballClubRepository footballClubRepository) {
+		return args -> {
+			footballClubRepository.save( new FootballClub("Arsenal", "ARS"));
+		};
 	}
 
 }
