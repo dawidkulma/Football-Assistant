49781afa173474691a4d29d6ebb8f469ec30f4a3
Patryk Wegrzy <wegpat@gmail.com>
2019-05-10 02:02:52 +0200

    Add Docker, SonarCloud, CovTests and auto-deployment

---
+++ src/test/java/com/football/assistant/SimpleTests.java
@@ -0,0 +1,13 @@
+package com.football.assistant;
+
+import org.junit.Test;
+import static junit.framework.TestCase.assertTrue;
+
+public class SimpleTests {
+
+    @Test
+    public void simpleTest() throws Exception {
+        assertTrue(true);
+    }
+
+}
