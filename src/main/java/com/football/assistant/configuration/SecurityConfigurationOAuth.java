95e9a0540bd8dcd7475ef99ae30d5e1ff1e09f91
dawidkulma <dawidkulma@gmail.com>
2019-07-16 21:54:43 +0200

    Update security config

--- src/main/java/com/football/assistant/configuration/SecurityConfigurationOAuth.java
+++ src/main/java/com/football/assistant/configuration/SecurityConfigurationOAuth.java
@@ -1,4 +1,33 @@
 package com.football.assistant.configuration;
 
-public class SecurityConfigurationOAuth {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@Order(2)
+public class SecurityConfigurationOAuth extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.antMatchers("/features").permitAll()
+				.antMatchers("/api_info").permitAll()
+				.antMatchers("/login").permitAll()
+				.antMatchers("/registration").permitAll()
+				.antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
+				.authenticated()
+				.and().csrf().disable().formLogin()
+				.loginPage("/login").failureUrl("/login?error=true")
+				.defaultSuccessUrl("/home")
+				.and()
+				.oauth2Login()
+				.and().logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/").and().exceptionHandling()
+				.accessDeniedPage("/access-denied");
+	}
 }
