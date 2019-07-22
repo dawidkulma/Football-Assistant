aacb510d588ac5a22c92d81ab6c7266cc6208792
daffcio <dawidkulma@gmail.com>
2019-07-23 00:09:20 +0200

    Add first searching integration

--- src/main/java/com/football/assistant/controller/SearchController.java
+++ src/main/java/com/football/assistant/controller/SearchController.java
@@ -1,5 +1,6 @@
 package com.football.assistant.controller;
 
+import com.football.assistant.api.ApiManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,16 @@ public class SearchController {
     @RequestMapping(value = "/search", method = RequestMethod.POST)
     public String search(@RequestParam("phrase") String phrase){
 
-        return "redirect: ...";
+        ApiManager apiManager = new ApiManager();
+        String itemId;
+        if((itemId = apiManager.searchLeagueByName(phrase)) != null){
+            return "redirect:/league/view/" + itemId;
+        } else if((itemId = apiManager.searchTeamByName(phrase)) != null){
+            return "redirect:/football_club/view/" + itemId;
+        } else if((itemId = apiManager.searchPlayerByName(phrase)) != null){
+            return "redirect:/player/view/" + itemId;
+        } else{
+            return "redirect: not_found";
+        }
     }
 }
