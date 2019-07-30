5fefe4f0fb518e6cedf61e6471565f360e3de444
daffcio <dawidkulma@gmail.com>
2019-07-30 21:23:56 +0200

    Add comments display at user account

--- src/main/java/com/football/assistant/controller/AccountController.java
+++ src/main/java/com/football/assistant/controller/AccountController.java
@@ -1,5 +1,6 @@
 package com.football.assistant.controller;
 
+import com.football.assistant.domain.PostComment;
 import com.football.assistant.domain.User;
 import com.football.assistant.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(path = "/profile")
@@ -24,6 +30,15 @@ public class AccountController {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findUserByEmail(auth.getName());
         modelAndView.addObject("user", user);
+
+        Set<PostComment> postComments = user.getPostsComments();
+        if (postComments == null){
+            modelAndView.setViewName("not_found");
+            return modelAndView;
+        }
+        List<PostComment> comments = postComments.stream().sorted(Comparator.comparing(PostComment::getCreationTimestamp)).collect(Collectors.toList());
+        modelAndView.addObject("comments", comments);
+
         modelAndView.setViewName("profile");
         return modelAndView;
     }
